9b9314957323ff6ea02d5f6c06c838636bfd155f
sumon paul <sumpaul100@gmail.com>
2022-12-15 20:06:09 +0600

    event handle

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ const App = () =>{
 
   return(
     <div>
-      <Clock locale='en-US'/>
+      <Clock/>
     </div>
   )
 }
--- src/Components/Clock/Clock.js
+++ src/Components/Clock/Clock.js
@@ -2,23 +2,36 @@ import React from 'react';
 import "./clock.css";
 
 class Clock extends React.Component{
-state = {date: new Date()};
+    state = {date: new Date(), locale:'en-US'};
+    // constructor(props){
+    //     super(props);
+    //     this.state = {date: new Date(), locale:'en-US'};
+    //     this.handleclick = this.handleclick.bind(this); // we can use arrow functions instead of bind;
+    // }
+   
    componentDidMount(){
        this.clockTimer = setInterval(() => {this.tick();}, 1000);
     }
     componentWillUnmount(){
         clearInterval(this.clockTimer)
     }
+   handleclick(locale){
+    //e.preventDefault()//a tag has default behavior that's why used preventDefault function 
+   this.setState({locale});
+   }
+
    tick(){
     this.setState({date: new Date()});
    }
-
-
     render(){
+        const {date, locale} = this.state;
         return(
             <div className='watch'>
                 <h3>Smart Watch</h3>
-               <span> {this.state.date.toLocaleTimeString(this.props.locale)}</span>
+               <span> {date.toLocaleTimeString(locale)}</span>
+               {/* <button style={{padding: '5px 10px',display: 'block',margin: '20px auto'}} type='button' onClick={this.handleclick}>Switch lang</button> */}
+<br></br>
+               <button onClick={()=>this.handleclick("bn-BD")} style={{display: 'inline-block', color: 'black',background: 'cyan',padding:'5px 10px'}}>Click here</button>
             </div>
         )
     }
