7162729102abc82fef562ae2cd7f6d1704cbe323
sumon paul <sumpaul100@gmail.com>
2022-12-18 00:11:23 +0600

    we shouldn't use index as a key. we should use an making key through Math.random(); if we don't have

--- src/Components/ClockList/ClockList.js
+++ src/Components/ClockList/ClockList.js
@@ -5,8 +5,8 @@ const ClockList = ({quantity = []}) => {
     
     return (
         <div className='border'>
-            {quantity.map((key,index) => <Clock key={index}/>)}
-            
+            {quantity.map((key,index) => <Clock key={Math.random()}/>)} 
+            {/* we shouldn't use index as a key. we should use an making key through Math.random(); if we don't have */}
         </div>
     );
 };
