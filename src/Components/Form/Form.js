e7cc62187af78202513fbf8421e8b227dd0c6b44
sumon paul <sumpaul100@gmail.com>
2022-12-20 00:57:49 +0600

    controled textarea using same onChange function by if else condition

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -1,20 +1,30 @@
-import React from 'react';
+import React from "react";
 
 export default class Form extends React.Component {
-    state = {
-        title: 'Javascript'
-    }
+  state = {
+    title: "Javascript",
+    textareatext: "textarea text is changeing",
+  };
 
-    handlechange = (event)=>{
-        console.log(event.target.value);
-        this.setState({title: event.target.value})
+  handlechange = (event) => {
+    if (event.target.type === "text") {
+      this.setState({ title: event.target.value });
+    } else if (event.target.type === "textarea") {
+      this.setState({ textareatext: event.target.value });
+    }else{
+        console.log('nothing here')
     }
-    render() {
-        const { title } = this.state;
-        return (
-            <div>
-                <input type="text" value={title} onChange={this.handlechange}/>
-            </div>
-        );
-    }
+  };
+  render() {
+    const { title,textareatext} = this.state;
+    return (
+      <div>
+        <input type="text" value={title} onChange={this.handlechange} />
+        <br />
+        <br />
+        <textarea name="textarea" value={textareatext} onChange={this.handlechange}></textarea>
+        {/* we shouldn't use inside the textarea in react ..... we should use value as type text*/}
+      </div>
+    );
+  }
 }
