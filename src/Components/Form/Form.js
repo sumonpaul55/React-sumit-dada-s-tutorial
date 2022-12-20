e46b0c1c5a469ee77e854c689bc323d8c1746539
sumon paul <sumpaul100@gmail.com>
2022-12-21 00:03:44 +0600

    onSubmit is working forms data sent in console

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -5,7 +5,7 @@ export default class Form extends React.Component {
     title: "Javascript",
     textareatext: "textarea text is changeing",
     library: "Ract",
-    checkedbox: false
+    checkedbox: false,
   };
 
   handlechange = (event) => {
@@ -16,35 +16,42 @@ export default class Form extends React.Component {
     } else if (event.target.type === "textarea") {
       this.setState({ textareatext: event.target.value });
     } else if (event.target.type === "select-one") {
-      this.setState({ library: event.target.value,
-      });
+      this.setState({ library: event.target.value });
     } else if (event.target.type === "checkbox") {
-      this.setState({ checkedbox: event.target.checked,
-      });
-    }
-    else {
+      this.setState({ checkedbox: event.target.checked });
+    } else {
       console.log("nothing here");
     }
   };
+  submitHandler=(event)=>{
+    const { library, textareatext , title, checkedbox} = this.state;
+    event.preventDefault();
+    console.log( title, textareatext ,library,  checkedbox)
+  }
+
+
   render() {
     const { title, textareatext, library, checkedbox } = this.state;
     return (
       <div>
-        <input type="text" value={title} onChange={this.handlechange} />
-        <br />
-        <br />
-        <textarea name="textarea" value={textareatext} onChange={this.handlechange}></textarea>
-        {/* we shouldn't use inside the textarea in react ..... we should use value as type text*/}
-        <div>
-          <select value={library} onChange={this.handlechange}>
-            <option value="React">React</option>
-            <option value="Angular">Angular</option>
-            <option value="Vue js">Vue js</option>
-          </select>
-          <br /><br />
-          <input type="checkbox" checked={checkedbox} onChange={this.handlechange} name="" id="" />
-        </div>
-
+        <form onSubmit={this.submitHandler}>
+          <input type="text" value={title} onChange={this.handlechange} />
+          <br />
+          <br />
+          <textarea name="textarea" value={textareatext} onChange={this.handlechange}></textarea>
+          {/* we shouldn't use inside the textarea in react ..... we should use value as type text*/}
+          <div>
+            <select value={library} onChange={this.handlechange}>
+              <option value="React">React</option>
+              <option value="Angular">Angular</option>
+              <option value="Vue js">Vue js</option>
+            </select>
+            <br />
+            <input type="checkbox" checked={checkedbox} onChange={this.handlechange} name="" id="" />
+            <br />
+            <input type="submit" />
+          </div>
+        </form>
       </div>
     );
   }
