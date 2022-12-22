e35855e321216a050cf8c931e4bc10318e982a05
sumon paul <sumpaul100@gmail.com>
2022-12-22 13:02:25 +0600

    temparature aplication is ready

--- src/Components/Checktemparature/Checktemparature.js
+++ src/Components/Checktemparature/Checktemparature.js
@@ -1,25 +1,26 @@
 import React from "react";
+import { convert, toCelcius, tofahreheit } from "../../lib/Converter";
 import './CheckTemparature.css';
 import TemparatureInput from "./TemparatureInput";
 
 export default class CheckTemparature extends React.Component {
     state = {
         temparature: '',
-        scale: 'c'  
+        scale: 'c'
     }
 handleChange = (e, scale) => {
     this.setState({
         temparature: e.target.value,
         scale,
-        
     });
 } 
     render(){   
         const {temparature,scale} = this.state;   
+        const celcius = scale === 'c'? convert(temparature, tofahreheit): temparature;
+        const Fahrenheit = scale === 'f'? convert(temparature, toCelcius): temparature;
         return <div className="temparatureinputfield">
-            <TemparatureInput scale='c'/>
-            <TemparatureInput scale='f'/>
-
+            <TemparatureInput scale='c' temparature={Fahrenheit} OntemparatureChange={this.handleChange}/>
+            <TemparatureInput scale='f'temparature={celcius} OntemparatureChange={this.handleChange}/>
         </div>
     }
 }
--- src/Components/Checktemparature/TemparatureInput.js
+++ src/Components/Checktemparature/TemparatureInput.js
@@ -1,16 +1,15 @@
 import React from "react";
 import './CheckTemparature.css';
 
-
 const scaleName ={
     c: 'Celcius',
     f: 'Fahrenheit'
 }
-export default function TemparatureInput({temparature,scale, OntemparatureChange}){
+export default function TemparatureInput({temparature, scale, OntemparatureChange}){
         return (
             <fieldset>
                 <legend>Enter Temparature in {scaleName[scale]}</legend>
-                <input type="text" value={temparature} onChange={OntemparatureChange} />
+                <input type="text" value={temparature} onChange={(e)=>OntemparatureChange(e,scale)} />
         </fieldset>
         )
 }
