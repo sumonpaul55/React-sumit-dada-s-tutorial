71975cefb14821d574ff669e0664506405afa1af
sumon paul <sumpaul100@gmail.com>
2022-12-22 11:43:09 +0600

    writhe a convert function in lib folder to convert the value whatever it Celcius or fahreheit

--- src/Components/Checktemparature/Checktemparature.js
+++ src/Components/Checktemparature/Checktemparature.js
@@ -1,28 +1,25 @@
 import React from "react";
-import Calculator from "./Calculator/Calculator";
 import './CheckTemparature.css';
+import TemparatureInput from "./TemparatureInput";
 
 export default class CheckTemparature extends React.Component {
-    state={
-        temparature: ''
+    state = {
+        temparature: '',
+        scale: 'c'  
     }
-
-    changeTemvalue=(e) => {
-        this.setState({
-            temparature: e.target.value, 
-        })
+handleChange = (e, scale) => {
+    this.setState({
+        temparature: e.target.value,
+        scale,
         
-    }
-
-    render(){
-        const {temparature} = this.state;
+    });
+} 
+    render(){   
+        const {temparature,scale} = this.state;   
         return <div className="temparatureinputfield">
-            <fieldset>
-                <legend>Enter Temparature in Selcius</legend>
-                <input type="text" value={temparature} onChange={this.changeTemvalue} />
-            </fieldset>
-            <h4>Result: </h4>
-            <Calculator selcius={parseFloat(temparature)}/>
+            <TemparatureInput scale='c'/>
+            <TemparatureInput scale='f'/>
+
         </div>
     }
 }
---
+++ src/Components/Checktemparature/TemparatureInput.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './CheckTemparature.css';
+
+
+const scaleName ={
+    c: 'Celcius',
+    f: 'Fahrenheit'
+}
+export default function TemparatureInput({temparature,scale, OntemparatureChange}){
+        return (
+            <fieldset>
+                <legend>Enter Temparature in {scaleName[scale]}</legend>
+                <input type="text" value={temparature} onChange={OntemparatureChange} />
+        </fieldset>
+        )
+}
