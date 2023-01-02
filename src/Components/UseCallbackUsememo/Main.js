79bdfd4562c65dc9e292cbc89bcf019206ed5f46
sumon paul <sumpaul100@gmail.com>
2023-01-03 01:56:54 +0600

    çostly operation take more time to load and effect whole component . useMemo will solve this problem where it not effect there no to time. and we must use defedancy

--- src/Components/UseCallbackUsememo/Main.js
+++ src/Components/UseCallbackUsememo/Main.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useMemo, useState } from 'react';
 import Mybutton from './Mybutton';
 import Showcount from './Showcount';
 import Title from './Title';
@@ -14,11 +14,18 @@ const Main = () => {
     const increamentByFive = useCallback(() => {
         setCount2((prevcoutn)=> prevcoutn + 5)
     },[])
+    const isEvetOrod = useMemo(()=> {
+        let i =0;
+        while(i<500000000)i += 1 //costlty operaiton
+        return count1 % 2 === 0;
+    },[count1]);
     return ( 
         <div>
             <Title/>
             <Showcount count={count1} title='counte by 1' />
             <Mybutton handleclick={increamentByone}>Increment by one </Mybutton>
+
+            <button style={{marginTop: '5px'}}>{(isEvetOrod) ? 'Even' : 'Odd'}</button>
             <hr />
             <Showcount count={count2}  title='count by 5'/>
             <Mybutton handleclick={increamentByFive}>Increment by five</Mybutton>
