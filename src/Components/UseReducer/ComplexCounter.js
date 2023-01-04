04483a4ca3203c66d6835cc7ef6422a75e2ffa06
sumon paul <sumpaul100@gmail.com>
2023-01-04 13:12:00 +0600

    we must use spread operator if we use multiple value as initial value

--- src/Components/UseReducer/ComplexCounter.js
+++ src/Components/UseReducer/ComplexCounter.js
@@ -2,6 +2,7 @@ import React, { useReducer } from 'react';
 
 const initialState ={
     counter : 0,
+    counter2: 0,
 };
 
 const reducer = (state, action)=>{
@@ -9,9 +10,13 @@ const reducer = (state, action)=>{
   
     switch(action.type){
         case 'increament':
-            return {counter: state.counter + action.value};
+            return {...state,counter: state.counter + action.value};
         case 'decreaament':
-            return {counter: state.counter - action.value};
+            return {...state, counter: state.counter - action.value}; 
+        case 'increament2':
+            return {...state,counter2: state.counter2 + action.value};  
+        case 'decreaament2':
+            return {...state,counter2: state.counter2 - action.value};  
         default:
             return {counter: state.counter,};
     }
@@ -24,10 +29,14 @@ const ComplexCounter = () => {
         <div>
             <h2>Icreament & Decreament using useReducer</h2>
             <h3>Count : {count.counter}</h3>
+            <h3>Count : {count.counter2}</h3>
             <button type='button' onClick={()=> dispatch({type: 'increament', value: 1})}>Increament by 1</button>
             <button type='button' onClick={()=> dispatch({type: 'decreaament', value: 1})}>Dncreament by 1</button>
             <button type='button' onClick={()=> dispatch({type: 'increament', value: 5})}>Increament by 5</button>
             <button type='button' onClick={()=> dispatch({type: 'decreaament', value: 5})}>Dncreament by 5</button>
+            <button type='button' onClick={()=> dispatch({type: 'increament2', value: 2})}>Dncreament by 2 in counter2</button>
+            <button type='button' onClick={()=> dispatch({type: 'decreaament2', value: 2})}>Dncreament by 2 in counter2</button>
+
         </div>
     );
 };
