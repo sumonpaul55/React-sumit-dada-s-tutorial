1d6a217009f4c5d9a6f40bfa779f1fa469ed7882
sumon paul <sumpaul100@gmail.com>
2023-01-06 16:24:24 +0600

    If we need to reuse same functionality then we can create a custom hook. Of course it start with useHookName

---
+++ src/Components/Customhooks/useWindwoResize.js
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from 'react';
+
+const useWindwoResize = () => {
+    const [onsmallscreen, setonsmallscreen] = useState(false);
+
+   const checkscreanSize = ()=>{
+        setonsmallscreen(window.innerWidth < 768)   ;
+    }
+
+    useEffect(()=>{
+        checkscreanSize();
+        window.addEventListener('resize', checkscreanSize);
+
+        // eslint-disable-next-line no-restricted-globals
+        return () => removeEventListener('resize', checkscreanSize);
+    })
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default useWindwoResize;
