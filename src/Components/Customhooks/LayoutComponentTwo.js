618a0abb5eec2850c373a49601796f476aef1d33
sumon paul <sumpaul100@gmail.com>
2023-01-06 16:33:56 +0600

    class changing dynamically using custom hook. we should export the elements wich we need and import the component and declear a const with the name of elements wich is return and the call the component

--- src/Components/Customhooks/LayoutComponentTwo.js
+++ src/Components/Customhooks/LayoutComponentTwo.js
@@ -1,19 +1,7 @@
-import React, { useEffect, useState } from 'react';
+import useWindwoResize from "./useWindwoResize";
 
 const LayoutComponentTwo = () => {
-    const [onsmallscreen, setonsmallscreen] = useState(false);
-
-   const checkscreanSize = ()=>{
-        setonsmallscreen(window.innerWidth < 768)   ;
-    }
-
-    useEffect(()=>{
-        checkscreanSize();
-        window.addEventListener('resize', checkscreanSize);
-
-        // eslint-disable-next-line no-restricted-globals
-        return () => removeEventListener('resize', checkscreanSize);
-    })
+   const onsmallscreen = useWindwoResize();
     return (
         <div className={(onsmallscreen)? 'small': 'Large'}>
              <h1>Class changing dynamically</h1>
