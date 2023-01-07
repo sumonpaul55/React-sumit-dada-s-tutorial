b766a8107ab9fd81251db38dd2a07e935488e0a6
sumon paul <sumpaul100@gmail.com>
2023-01-08 00:29:33 +0600

    styled-components used. it changed the static class name and set a dynamic class. it use for specific styled component

--- src/Components/StyilingComponents/Container.style.js
+++ src/Components/StyilingComponents/Container.style.js
@@ -1,2 +1,11 @@
 import styled from "styled-components";
 
+const ContainerStyling = styled.div`
+width: 800px;
+margin: auto;
+padding: 20px;
+border-radius: 5px;
+background-color: #333;
+color: white;
+`
+export default ContainerStyling;
