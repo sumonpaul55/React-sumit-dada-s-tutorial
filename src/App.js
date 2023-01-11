61bc3fea7b8c02b1977931b68e0a4d05ed6d2e2b
sumon paul <sumpaul100@gmail.com>
2023-01-11 23:54:17 +0600

    navbar liking with component using Link

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import About from './Components/UseOfRouter/About';
-import Home from './Home';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import About from "./Components/UseOfRouter/About";
+import Navbar from "./Components/UseOfRouter/Navbar";
+import Servise from "./Components/UseOfRouter/Servise";
+import Home from "./Home";
 
 const App = () => {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path='/' element={<Home/>}/> 
-                <Route path='about' element={<About/>}/> 
-            </Routes>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+    <Navbar/>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="home" element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="service" element={<Servise />} />
+        </Routes>
+      </BrowserRouter>
+  );
 };
 
 export default App;
--- src/Components/UseOfRouter/Navbar.js
+++ src/Components/UseOfRouter/Navbar.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import About from './About';
-import Home from './Home';
-import Servise from './Servise';
 
 const Navbar = () => {
     return (
         <nav>
             <ul>
-                <li><Link to={<Home/>}>Home</Link></li>
-                <li><Link to={<About/>}>About</Link></li>
-                <li><Link to={<Servise/>}>Service</Link></li>
+                <li><Link to='/'>Home</Link></li>
+                <li><Link to='about'>About</Link></li>
+                <li><Link to='service'>Service</Link></li>
             </ul>
         </nav>
     );
